ffb66ff8152f1213558be5731adce3bbdb4180ed
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-24 16:58:04 +0300

    globalStyles

---
+++ globalStyles.ts
@@ -0,0 +1,14 @@
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Karla:400,700|Open+Sans:400,600');
+  ${reset}
+  body{
+    font-family: 'Open Sans', sans-serif;
+    font-size:14px;
+  }
+  h1,h2,h3,h4,h5,h6{
+    font-family: 'Karla', sans-serif;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import "../globalStyles";
 
 const Hello = styled.div`
   color: red;
